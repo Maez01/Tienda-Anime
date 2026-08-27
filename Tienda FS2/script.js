1abef2dbdb8bf6f879fb04b436dfec7d981a0afa
Maez01 <155858496+Maez01@users.noreply.github.com>
2026-08-27 10:47:53 -0400

    Se agregan cambios iniciales

---
+++ Tienda FS2/script.js
@@ -0,0 +1,23 @@
+//imagen 1
+function abrirImagen1() {
+    document.getElementById("visor1").style.display = "flex";
+}
+function cerrarImagen1() {
+    document.getElementById("visor1").style.display = "none";
+}
+
+//imagen 2
+function abrirImagen2() {
+    document.getElementById("visor2").style.display = "flex";
+}
+function cerrarImagen2() {
+    document.getElementById("visor2").style.display = "none";
+}
+
+//imagen 3
+function abrirImagen3() {
+    document.getElementById("visor3").style.display = "flex";
+}
+function cerrarImagen3() {
+    document.getElementById("visor3").style.display = "none";
+}
